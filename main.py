be0c9dbf265abecf38e4713282b44e032acb507e
Evan McCaleb <evanmccaleb@gmail.com>
2023-03-21 21:26:02 -0400

    KivyTut Added the ball statically

--- main.py
+++ main.py
@@ -2,9 +2,18 @@ import kivy
 
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.properties import NumericProperty, ReferenceListProperty
+from kivy.vector import Vector
 kivy.require('2.1.0')
 
 
+class PongBall(Widget):
+    velocity_x = NumericProperty(0)
+    velocity_y = NumericProperty(0)
+    velocity = ReferenceListProperty(velocity_x, velocity_y)
+
+    def move(self):
+        self.pos = Vector(*self.velocity) + self.pos
 
 class PongGame(Widget):
     pass
