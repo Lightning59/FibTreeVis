e1b9c422eb8c6ac13182ef5bf4deab4c85545769
Evan McCaleb <evanmccaleb@gmail.com>
2023-03-26 18:54:08 -0400

    Starting to make sense of things

---
+++ main.py
@@ -0,0 +1,30 @@
+from kivy.app import App
+from kivy.properties import ObjectProperty
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.widget import Widget
+
+
+
+class BasicQueue(BoxLayout):
+    pass
+
+class QueueBox(BoxLayout):
+    pass
+
+class MainLayout(Widget):
+    priority_queue = ObjectProperty()
+
+    def __init__(self, **kwargs):
+        super(MainLayout, self).__init__(**kwargs)
+
+        layout=self.ids.priority_queue
+        for i in range(50):
+            layout.add_widget(QueueBox())
+
+
+class FibTreeVisApp(App):
+    def build(self):
+        return MainLayout()
+
+
+FibTreeVisApp().run()
