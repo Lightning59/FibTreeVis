c819f2a49f96a2ec87718c10440f0ab3bd21d9b2
Evan McCaleb <evanmccaleb@gmail.com>
2023-03-29 22:31:40 -0400

    Pep8 Cleanup and minimal refactor

--- main.py
+++ main.py
@@ -1,53 +1,42 @@
 from kivy.app import App
-from kivy.properties import ObjectProperty, StringProperty, NumericProperty
+from kivy.properties import ObjectProperty, NumericProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.widget import Widget
 import priority_queue
 
 
-
-
 class BasicQueue(BoxLayout):
     pass
+
 
 class PHQueueBox(BoxLayout):
     pass
 
 
-
-
-
 class MainLayout(Widget):
     priority_queue = ObjectProperty()
-    std_height=100
-    std_min=25
-    curr_height=NumericProperty(100)
-
+    std_height = 100
+    std_min = 25
+    curr_height = NumericProperty(100)
 
     def __init__(self, **kwargs):
         super(MainLayout, self).__init__(**kwargs)
 
-        self.simulation = priority_queue.PriorityQueueProblem(20, 25, 75, 20, 40, (1, 100), (5,30))
+        self.simulation = priority_queue.PriorityQueueProblem(20, 25, 75, 20, 40, (1, 100), (5, 30))
 
-        self.main_row=self.ids.main_row
+        self.main_row = self.ids.main_row
         self.main_row.add_widget(PHQueueBox())
-        self.pqueue=BasicQueue()
+        self.pqueue = BasicQueue()
         self.main_row.add_widget(self.pqueue)
         self.main_row.add_widget(PHQueueBox())
 
+    #    def on_size (self, *args):
+    #        pass
 
-#    def on_size (self, *args):
-#        currhint=min(self.std_height,self.layout.height)
-#        if self.layout.height<100:
-#            currhint=max(25,self.layout.height)
-#        for widget in self.layout.children:
-#            widget.maxhint=currhint
-#        print(self.layout.height)
-    def forward_press(self,*args):
+    def forward_press(self, *args):
         self.simulation.do_next_step()
         if self.simulation.next_incoming is not None:
             self.pqueue.add_widget(self.simulation.next_incoming.return_vis())
-
 
     def backward_press(self, *args):
         self.main_row.add_widget(self.pqueue)
--- priority_queue.py
+++ priority_queue.py
@@ -6,17 +6,17 @@ from kivy.uix.boxlayout import BoxLayout
 
 
 class QueueBox(BoxLayout):
-    initial_label=StringProperty('init')
+    initial_label = StringProperty('init')
     curr_label = StringProperty('curr')
     maxhint = NumericProperty(100)
 
     def __init__(self, val, **kwargs):
-        super(QueueBox,self).__init__(**kwargs)
-        self.initial_label=str(val)
-        self.curr_label=str(val)
+        super(QueueBox, self).__init__(**kwargs)
+        self.initial_label = str(val)
+        self.curr_label = str(val)
 
-    def update_curr(self,val):
-        self.curr_label=str(val)
+    def update_curr(self, val):
+        self.curr_label = str(val)
 
     def on_size(self, *args):
         try:
@@ -25,26 +25,27 @@ class QueueBox(BoxLayout):
                 currhint = max(25, self.parent.height)
             self.maxhint = currhint
         except AttributeError:
-            self.maxhint=100
+            self.maxhint = 100
 
-class MessageObject():
-    def __init__(self, priority_range: (int, int) ) -> None:
-        self.priority=random.randint(priority_range[0], priority_range[1])
-        self.initial=self.priority
-        self.active=True
+
+class MessageObject:
+    def __init__(self, priority_range: (int, int)) -> None:
+        self.priority = random.randint(priority_range[0], priority_range[1])
+        self.initial = self.priority
+        self.active = True
         self.vis = QueueBox(self.priority)
 
-    def get_priority(self)-> int:
+    def get_priority(self) -> int:
         return self.priority
 
     def mark_inactive(self):
-        self.active=False
+        self.active = False
 
     def is_active(self) -> bool:
         return self.active
 
-    def decrease_key(self,dec: int) -> None:
-        if self.priority-dec>0:
+    def decrease_key(self, dec: int) -> None:
+        if self.priority - dec > 0:
             self.priority -= dec
             self.vis.update_curr(self.priority)
 
@@ -52,94 +53,85 @@ class MessageObject():
         return self.vis
 
 
-
-class PriorityQueue():
+class PriorityQueue:
 
     def __init__(self) -> None:
-        self.queue=[]
+        self.queue = []
 
-    def addmessage(self,msg: MessageObject ) -> None:
-        if len(self.queue)==0:
+    def addmessage(self, msg: MessageObject) -> None:
+        if len(self.queue) == 0:
             self.queue.append(msg)
         else:
-            for i in range(0,len(self.queue)):
-                if msg.get_priority()<self.queue[i].get_priority():
+            for i in range(0, len(self.queue)):
+                if msg.get_priority() < self.queue[i].get_priority():
                     self.queue.insert(i, msg)
                     return
             self.queue.append(msg)
 
-
-
-    def topmessage(self)-> MessageObject:
-        if len(self.queue)>0:
+    def topmessage(self) -> Optional[MessageObject]:
+        if len(self.queue) > 0:
             return self.queue[0]
         else:
             return None
 
-    def sendmessage(self)-> MessageObject:
-        if len(self.queue)>0:
+    def sendmessage(self) -> Optional[MessageObject]:
+        if len(self.queue) > 0:
             return self.queue.pop(0)
         else:
             return None
 
 
-
-
-class AgeQueue():
+class AgeQueue:
 
     def __init__(self) -> None:
-        self.queue=[]
+        self.queue = []
 
-    def addmessage(self,msg: MessageObject ) -> None:
+    def addmessage(self, msg: MessageObject) -> None:
         self.queue.append(msg)
 
-    def decrease_key(self,numkeys: int , dec: int) -> None:
-        if numkeys<len(self.queue):
-            i=0
-            while i<numkeys:
-                msg=self.queue.pop(0)
+    def decrease_key(self, numkeys: int, dec: int) -> None:
+        if numkeys < len(self.queue):
+            i = 0
+            while i < numkeys:
+                msg = self.queue.pop(0)
                 if msg.is_active():
                     msg.decrease_key(dec)
                     self.queue.append(msg)
-                    i+=1
-        elif len(self.queue)==0:
+                    i += 1
+        elif len(self.queue) == 0:
             return
         else:
-            l=len(self.queue)
-            for i in range(l):
+            queue_len = len(self.queue)
+            for i in range(queue_len):
                 msg = self.queue.pop(0)
                 if msg.is_active():
                     msg.decrease_key(dec)
                     self.queue.append(msg)
 
-    def decrease_key_dryrun(self,numkeys: int , dec: int) -> list[MessageObject]:
-        listout=[]
-        if numkeys<len(self.queue):
-            i=0
-            index=i
-            while i<numkeys:
-                msg=self.queue[index]
+    def decrease_key_dryrun(self, numkeys: int) -> list[MessageObject]:
+        listout = []
+        if numkeys < len(self.queue):
+            i = 0
+            index = i
+            while i < numkeys:
+                msg = self.queue[index]
                 listout.append(msg)
                 if msg.is_active():
-                    i+=1
-                    index+=1
+                    i += 1
+                    index += 1
                 else:
-                    index+=1
-        elif len(self.queue)==0:
+                    index += 1
+        elif len(self.queue) == 0:
             return []
         else:
-            return [self.queue]
+            return self.queue
         return listout
 
 
-
-
-
-
-class PriorityQueueProblem():
+class PriorityQueueProblem:
 
     def __init__(self, base_in_rate: int, base_out_rate: int, surge_in_rate: int, surge_in_start: int,
-                 surge_in_end: int, priority_range: tuple[int,int], decrease_key:tuple[int,int]) -> None:
+                 surge_in_end: int, priority_range: tuple[int, int], decrease_key: tuple[int, int]) -> None:
         """
         Class constructor for a priority queue simulation
 
@@ -154,77 +146,64 @@ class PriorityQueueProblem():
         :param decrease_key: (number of messages to reduce at time, Rate in times per 100 which this will occur)
         """
 
-        self.base_in_rate=base_in_rate
-        self.base_out_rate=base_out_rate
+        self.base_in_rate = base_in_rate
+        self.base_out_rate = base_out_rate
         self.surge_in_rate = surge_in_rate
         self.surge_in_start = surge_in_start
         self.surge_in_end = surge_in_end
-        self.timestep=0
+        self.timestep = 0
         self.rate_period = 100
         self.priority_range = priority_range
         self.next_incoming = None
         self.next_outgoing = None
-        self.next_decrease=[]
+        self.next_decrease = []
         self.priority_queue = PriorityQueue()
         self.age_queue = AgeQueue()
-        self.input_interval=int(self.rate_period/self.base_in_rate)
+        self.input_interval = int(self.rate_period / self.base_in_rate)
         self.output_interval = int(self.rate_period / self.base_out_rate)
         self.decrease_key_qty, self.decrease_key_period = decrease_key
-        self.decrease_key_interval= int(self.rate_period / self.decrease_key_period)
+        self.decrease_key_interval = int(self.rate_period / self.decrease_key_period)
 
-    def gen_next_message(self)->Optional[MessageObject]:
-        rate=self.base_in_rate
-        if self.surge_in_start<self.timestep<self.surge_in_end:
-            rate= self.surge_in_rate
-        r=random.randint(1,100)
-        if r<=rate:
-            priority=random.randint(self.priority_range[0],self.priority_range[1])
-            msg=MessageObject(self.priority_range)
+    def gen_next_message(self) -> Optional[MessageObject]:
+        rate = self.base_in_rate
+        if self.surge_in_start < self.timestep < self.surge_in_end:
+            rate = self.surge_in_rate
+        r = random.randint(1, 100)
+        if r <= rate:
+            msg = MessageObject(self.priority_range)
             return msg
         return None
-
-
-
-
-
 
     def do_next_step(self) -> None:
         # add incoming messages to queue and reset incoming queue
         if self.next_incoming is not None:
             self.priority_queue.addmessage(self.next_incoming)
             self.age_queue.addmessage(self.next_incoming)
-            self.next_incoming=None
-        # send outgoing messages reset outgoint mark message in time queue as inactive
+            self.next_incoming = None
+        # send outgoing messages reset outgoing mark message in time queue as inactive
         if self.next_outgoing is not None:
-            msg=self.priority_queue.sendmessage()
+            msg = self.priority_queue.sendmessage()
             msg.mark_inactive()
             self.next_outgoing = None
         # perform decrease
-        if len(self.next_decrease)>0:
-            self.age_queue.decrease_key(self.decrease_key_qty,1)
-            self.next_decrease=[]
+        if len(self.next_decrease) > 0:
+            self.age_queue.decrease_key(self.decrease_key_qty, 1)
+            self.next_decrease = []
 
         # generate next message in and out one step ahead.
-        self.next_incoming=self.gen_next_message()
+        self.next_incoming = self.gen_next_message()
         # next message out
-        if (self.timestep+1)%self.output_interval == 0:
-            self.next_outgoing=self.priority_queue.topmessage()
-        #next decrease step
-        if (self.timestep+1)%self.decrease_key_interval == 0:
-            self.next_decrease=self.age_queue.decrease_key_dryrun(self.decrease_key_qty,1)
+        if (self.timestep + 1) % self.output_interval == 0:
+            self.next_outgoing = self.priority_queue.topmessage()
+        # next decrease step
+        if (self.timestep + 1) % self.decrease_key_interval == 0:
+            self.next_decrease = self.age_queue.decrease_key_dryrun(self.decrease_key_qty)
 
         self.timestep += 1
 
 
+if __name__ == "__main__":
+    x = PriorityQueueProblem(50, 5, 75, 20, 30, (1, 100), (5, 30))
 
-
-
-
-
-
-if __name__=="__main__":
-    x=PriorityQueueProblem(50, 5, 75, 20, 30, (1, 100), (5,30))
-
-    for i in range(200):
+    for trial_step in range(200):
         x.do_next_step()
-
